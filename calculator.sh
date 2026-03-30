8edd059c1ddf2608824594edbb9fe617174eff8b
Valery <Nyoh Jonathan Smith>
2026-03-30 12:21:28 +0100

    chore: adding content to calculator.sh file

--- calculator.sh
+++ calculator.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+read -p "Enter first number: " num1
+read -p "Enter operator: " op
+read -p "Enter second number: " num2
+
+case $op in 
+'+') result=$(($num1 + $num2))
+     echo $result
+     ;;
+'-') result=$(($num1 - $num2))
+     echo $result
+     ;;
+'*') result=$(($num1 * $num2))
+     echo $result
+     ;;
+'/') if (( $num2 == 0 )); then 
+         echo "Cant be performed"
+     else
+       result=$(( $num1 / $num2))
+       echo $result
+     fi
+     ;;
+'#') echo "Unknown operator"
+        ;;
+esac
